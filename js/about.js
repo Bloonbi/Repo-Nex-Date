0da7332fb4a9bf4ce08e72d01d81132beee24885
Manuel Freitas <145071953+Bloonbi@users.noreply.github.com>
2024-10-03 13:17:01 -0300

    Delete js directory

--- js/about.js
+++
@@ -1,7 +0,0 @@
-const e = document.getElementById("email");
-const hrefe = document.getElementById("hrefe");
-var em= "marcio@gmail.com";
-
-e.textContent=em;
-
-hrefe.href=`mailto:${em}`;
--- js/cookies.js
+++
@@ -1,8 +0,0 @@
-
-const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
-    const [name, value] = cookie.split('=');
-    acc[name] = value;
-    return acc;
-  }, {});
-  
-  console.log(cookies['email']);  
